62a23bb61ad557f408d8768858a4b5e8ffe51c40
emanmkhareez <emkhareez19@gmail.com>
2021-05-23 19:03:30 -0700

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      Query: ''
+    }
+  }
+ 
+changeLoc = (e) =>{
+  e.preventDefault()
+
+  this.setState({
+   
+    Query:e.target.value
+  });
+  console.log(this.state.Query);
 }
+  getLoc = async () => {
 
+    let locUrl = `https://us1.locationiq.com/v1/reverse.php?key=pk.0070e91c94da8e671b4a2f75afb0072a=${this.state.Query}=LONGITUDE&format=json`;
+    let req = await axios.get(locUrl);
+    console.log(req);
+
+  }
+  render() {
+    return (
+      <>
+        <h2>city-exploer</h2>
+
+        <form onSubmit={this.getLoc}>
+          <label>enter city name</label>
+
+          <input type="text" onChange={this.changeLoc} />
+
+          <input type="submit" value="GetLocation" />
+        </form>
+
+      </>
+    )
+  }
+}
 export default App;
