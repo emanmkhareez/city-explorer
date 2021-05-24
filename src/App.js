960770b819f802b895209a587a9cfcc9b3b20842
emanmkhareez <emkhareez19@gmail.com>
2021-05-23 20:45:02 -0700

    add2

--- src/App.js
+++ src/App.js
@@ -7,25 +7,33 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      Query: ''
+      Query: '',
+      locationData:'',
+      show:false,
+      error:false,
     }
   }
  
-changeLoc = (e) =>{
-  e.preventDefault()
+changeLoc = (event) =>{
+ 
 
   this.setState({
    
-    Query:e.target.value
+    Query:event.target.value
   });
-  console.log(this.state.Query);
+  
 }
-  getLoc = async () => {
-
+  getLoc = async (event) => {
+    event.preventDefault();
     let locUrl = `https://us1.locationiq.com/v1/reverse.php?key=pk.0070e91c94da8e671b4a2f75afb0072a=${this.state.Query}=LONGITUDE&format=json`;
+    
     let req = await axios.get(locUrl);
-    console.log(req);
-
+    console.log(req[0]);
+this.setState({
+  locationData:locUrl[0],
+  show:true
+})
+console.log(this.state.Query);
   }
   render() {
     return (
@@ -38,7 +46,11 @@ changeLoc = (e) =>{
           <input type="text" onChange={this.changeLoc} />
 
           <input type="submit" value="GetLocation" />
+          <p>{this.state.locationData.display_name}</p>
         </form>
+        {this.state.show &&
+        <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.0070e91c94da8e671b4a2f75afb0072a&center=${this.state.locationData.lat},${this.state.locationData.lon}`} alt=''/>
+        }
 
       </>
     )
