daa836cb74e572e854b72fdf529224bb5cab1e6a
emanmkhareez <emkhareez19@gmail.com>
2021-07-04 16:32:03 -0700

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import axios from 'axios';//npm i axios
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'react-bootstrap/Image';
