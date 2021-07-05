f189a414ff7687a5f18bc6d7f64a43baf013e02b
emanmkhareez <emkhareez19@gmail.com>
2021-07-04 18:40:00 -0700

    add

--- src/App.js
+++ src/App.js
@@ -1,146 +1,122 @@
-import React from 'react';
-import axios from 'axios';//npm i axios
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Image from 'react-bootstrap/Image';
-import Show from './Show';
-let weatherMap=[]
-class App extends React.Component {
+import React from 'react'
+import axios from 'axios'//npm i axios
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Table from 'react-bootstrap/Table'
+import {Form,Button} from 'react-bootstrap/'
+
+class App extends React.Component{
   constructor(props){
-    super(props);
+    super(props)
+
     this.state={
-      weatherInfo:[]
-      
+      cityData:{},
+      cityName:'',
+      showMap:false,
+      errorMessage:false
+
     }
   }
-  
-      
+
+//function to get city from API locationIQ
+ getloc= async(e)=>{
+   e.preventDefault()
+    await this.setState({
+     cityName:e.target.city.value,
+     showMap:true
+
+   })
+   console.log(this.state.cityName);
+
+   try{let location=`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_Key}&q=${this.state.cityName}&format=json`;
+
+    let  sendReq= await axios.get(location)
+
+   console.log(sendReq.data);
+   this.setState({
+     cityData:sendReq.data[0]
+
+   })
+  }catch{
+    this.setState({
+      errorMessage:true
+    })
     
-
-  // constructor(props) {
-  //   super(props)
-
-  //   this.state = {
-  //     Query: '',
-  //     locationData:'',
-  //     show:false,
-  //     error:false,
-  //   }
-  // }
- 
-// changeLoc = (event) =>{
- 
-
-//   this.setState({
-   
-//     Query:event.target.value
-//   });
-  
-// }
-
-
-
-  getLoc = async () => {
-    // event.preventDefault();
-    
-let serverRout=process.env.REACT_APP_SERVER
-let locUrl=`${serverRout}/weather?cityName=Seattle&lon=-122.33207&lat=47.60621`;
-
-let req = await axios.get(locUrl);
-
-this.setState({
-  weatherInfo:req.data
-})
-
-    
-   console.log(req.data);
- console.log(this.state.weatherInfo);
 
   }
-  
-    // let locUrl = `https://eu1.locationiq.com/v1/search.php?key=pk.0070e91c94da8e671b4a2f75afb0072a&q=${this.state.Query}&format=json`
 
-//     try {
-
-//     let req = await axios.get(locUrl);
-    
-//    console.log(req[0]);
-   
-// this.setState({
-//   locationData:req.data[0],
-//   show:true
-// });
-// console.log(this.state.locationData);
+ }
 
 
-//     }
-//     catch{
-//       this.setState({
-//         show:false,
-//         error:true
-//       })
-//     }
-
-  
-
-
-  render() {
-
-
-    
-    return (
-
-      <>
-      <button onClick={this.getLoc}>weather</button>
-        
-
-
-
-{this.state.weatherInfo.map((item,idx)=>{
+ 
+  render(){
     return(
-        <Show
-        desc={item.description}
-                    date={item.date}
-                  
-                    />
-    )
-})
-    }
+      <>
+      <br></br>
 
+       <h1 style={{textAlign:'center'}}>City Explorer</h1>
 
+{/* form to enter city  */}
 
+       <Form onSubmit={this.getloc}>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    
+    <Form.Control type="text" placeholder="Enter city name" name="city" placeholder="enter city Name" style={{width:'200px'}} />
+    <Button variant="primary" type="submit" style={{float:'right',marginRight:'75em',marginTop:'-36px'}} >
+    Submit
+  </Button>
+  </Form.Group>
 
-
-
-
-     
-     
-       
-
-        {/* <form onSubmit={this.getLoc}>
-          <label>enter city name</label>
-
-          <input type="text" onChange={this.changeLoc} />
-
-          <input type="submit" value="GetLocation" />
-          <p>{this.state.locationData.display_name}</p>
-        </form>
-        {this.state.show &&
-
-
-<Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.0070e91c94da8e671b4a2f75afb0072a&center=${this.state.locationData.lat},${this.state.locationData.lon}`}  fluid />
-
-       
-        }
-
-{this.state.error &&
   
-        <p>error in getting thr data</p>
-      }  */}
+</Form>
 
 
+
+<br></br>
+<br></br>
+<br></br>
+
+
+
+
+
+{/* 
+table to render info to city */}
+
+
+       <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>City Name</th>
+      <th>latitude</th>
+      <th>longitude</th>
+    
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>{this.state.cityData.display_name}</td>
+      <td>{this.state.cityData.lat}</td>
+      <td>{this.state.cityData.lon}</td>
+     
+    </tr>
+    
+    
+  </tbody>
+</Table>
+{/* render map to the city */}
+{this.state.showMap&&<img alt='' src={`https://maps.locationiq.com/v3/staticmap?key=pk.0070e91c94da8e671b4a2f75afb0072a&center=${this.state.cityData.lat},${this.state.cityData.lon}`} style={{width:'400px',height:'400px'}} />
+}
+{this.state.errorMessage&&<div style={{color:'red'}}>Error in getting the data</div>}
+
+
+
+    
       </>
     )
   }
 }
-export default App;
+    
+
+  export default  App
